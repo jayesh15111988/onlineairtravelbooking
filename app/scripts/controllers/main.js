f486303020f9a31993d53e5c70ee101161fe1038
Jayesh Kawli <j.kawli@gmail.com>
2014-02-17 20:10:35 -0500

    Added clean support to add or show modal view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,14 +9,47 @@ var airlinetravelmodule=angular.module('airtravelbookingappApp')
     ];
   });
 
+
+airlinetravelmodule.directive('registerFirstpage', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attr) {
+            scope.dismiss = function() {
+                element.modal('hide');
+            };
+            scope.show = function() {
+                element.modal('show');
+            };
+
+        }
+    }
+});
+
+
+airlinetravelmodule.directive('registerSecondpage', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attr) {
+            scope.show = function() {
+                element.modal('show');
+            };
+            scope.dismiss = function() {
+                element.modal('hide');
+            };
+        }
+    }
+});
+
 airlinetravelmodule.controller('samcontroller',function($scope){
 
 
 
     $scope.dataclicked=function(){
-        alert("datareceived");
-        $('#registerview').modal('hide');
-        $('#gotoregistrationsecondpage').modal('show');
+        //alert("datareceived");
+        $scope.dismiss();
+       $scope.show();
+       // $('#registerview').modal('hide');
+       // $('#gotoregistrationsecondpage').modal('show');
 
     }
 })
