243d1ace802ddaaa28bb47eee98ab77f73a245f7
Jayesh Kawli <j.kawli@gmail.com>
2014-03-02 19:45:02 -0500

    some unsuccessful attempts to connect to remote api past crowss site exception

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,7 +5,7 @@
 
 
 var airlinetravelmodule=angular.module('airtravelbookingappApp');
-airlinetravelmodule.controller('MainCtrl', function ($scope,$http) {
+airlinetravelmodule.controller('MainCtrl', function ($scope) {
 
   });
 
@@ -151,73 +151,64 @@ airlinetravelmodule.controller('flightsearchcontroller',function($scope,$http){
 })*/
 
 
-    // Create the XHR object.
-    function createCORSRequest(method, url) {
-        var xhr = new XMLHttpRequest();
-        if ("withCredentials" in xhr) {
-            // XHR for Chrome/Firefox/Opera/Safari.
-            xhr.open(method, url, true);
-        } else if (typeof XDomainRequest != "undefined") {
-            // XDomainRequest for IE.
-            xhr = new XDomainRequest();
-            xhr.open(method, url);
-        } else {
-            // CORS not supported.
-            xhr = null;
-        }
-        return xhr;
-    }
-
-// Helper method to parse the title tag from the response.
-    function getTitle(text) {
-    //    return text.match('<title>(.*)?</title>')[1];
-    return "ad";
-    }
-
-// Make the actual CORS request.
-    function makeCorsRequest() {
-        // All HTML5 Rocks properties support CORS.
-
-        var url ="http://www.cs.indiana.edu/cgi-pub/jkawli/jayeshkawli/db_info.php";
-       // var url ="http://jayeshkawli.com/calculation.php";
-        //xmlhttp.open("GET","http://jayeshkawli.com/calculation.php",true);
-        //xmlhttp.send();
-        var xhr = createCORSRequest('GET', url);
-        if (!xhr) {
-            alert('CORS not supported');
-            return;
-        }
-
-        // Response handlers.
-        xhr.onload = function() {
-            var text = xhr.responseText;
-            var title = getTitle(text);
-            alert('Response from CORS request to ' + xhr.responseText + ': ' + title);
-        };
-
-        xhr.onerror = function() {
-            alert('Woops, there was an error making the request.');
-        };
-
-        xhr.send();
-    
-
-
-}
 
 
 airlinetravelmodule.controller('flightsearchcontroller',function($scope,$http){
     $scope.getAirports=function(){
-//var url = "https://api.flightstats.com/flex/airports/rest/v1/json/active?appId=9738bcd8&appKey=6c713890a9bf2822f783ab8870332617";
-        makeCorsRequest();
-  /*
-$http.jsonp(url).
-    success(function(data, status, headers, config) {
-        //what do I do here?
-    }).
-    error(function(data, status, headers, config) {
-        //$scope.error = true;
-        alert("eroror");
-    });
-    }*/
+
+
+     /*   $http({
+            method: 'GET',
+            url: 'https://api.flightstats.com/flex/airports/rest/v1/json/active',
+            params: { appId : '9738bcd8', appKey : '6c713890a9bf2822f783ab8870332617' }
+        })
+*/
+     /*   $http({
+            method: 'GET',
+            url  var url = "https://api.flightstats.com/flex/airports/rest/v1/json/active";
+
+
+      var config = {headers:  {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Accept':'*',
+      'Content-Type':'application/json;charset=UTF-8'
+
+      },
+      params:{
+      'appId':'9738bcd8',
+      'appKey':'6c713890a9bf2822f783ab8870332617'
+      }
+      };
+
+      $http.get(url,config).
+      success(function(data) {
+
+      }).
+      error(function(data) {
+      alert("Error --");
+      });: 'https://api.flightstats.com/flex/airports/rest/v1/json/active',
+            params: { appId : '9738bcd8', appKey : '6c713890a9bf2822f783ab8870332617' }.
+            success(function(data, status, headers, config) {
+                // this callback will be called asynchronously
+                // when the response is available
+            }).
+            error(function(data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            })});*/
+    /*    $http({method: 'GET', url: 'http://api.flightstats.com/flex/airports/rest/v1/json/active',
+             params: { appId : '9738bcd8', appKey : '6c713890a9bf2822f783ab8870332617' }
+        }).
+            success(function(data, status, headers, config) {
+                // this callback will be called asynchronously
+                // when the response is available
+            }).
+            error(function(data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
+*/
+
 }});
